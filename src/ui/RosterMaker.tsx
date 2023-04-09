20a6fd3d70c394dc6c41b0ad2a11e06eba68f0f7
Pujit Mehrotra <pmehrotra2@babson.edu>
2023-04-09 14:06:44 -0400

    inc: drag overlay

--- src/ui/RosterMaker.tsx
+++ src/ui/RosterMaker.tsx
@@ -7,6 +7,8 @@ import {
   PointerSensor,
   useSensor,
   useSensors,
+  useDroppable,
+  DragOverlay,
 } from "@dnd-kit/core";
 import {
   arrayMove,
@@ -16,7 +18,7 @@ import {
   useSortable,
 } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
-import { useState } from "react";
+import { PropsWithChildren, useState } from "react";
 
 export function SortableItem(props: { id: string }) {
   const { attributes, listeners, setNodeRef, transform, transition } =
@@ -33,15 +35,61 @@ export function SortableItem(props: { id: string }) {
       style={style}
       {...attributes}
       {...listeners}
-      className="bg-green-300 px-4 py-2"
+      className="rounded-md bg-neutral-50 px-4 py-2"
     >
       An Item {props.id}
     </div>
   );
 }
 
+const warGroups = [
+  "G1",
+  "G2",
+  "G3",
+  "G4",
+  "G5",
+  "G6",
+  "G7",
+  "G8",
+  "G9",
+  "G10",
+  "Reserve",
+] as const;
+
+// https://steveholgado.com/typescript-types-from-arrays/
+type TWarGroup = (typeof warGroups)[number];
+type TDraggableItems = Record<TWarGroup, string[]>;
+
+function GroupContainer(props: PropsWithChildren<{ id: string }>) {
+  // Enables dragging items into empty list
+  const { setNodeRef } = useDroppable({
+    id: props.id,
+  });
+  return (
+    <div
+      ref={setNodeRef}
+      className="space-y-4 rounded-lg bg-neutral-200 p-4 shadow-lg"
+    >
+      {props.children}
+    </div>
+  );
+}
+
 export default function RosterMaker() {
-  const [items, setItems] = useState(["1", "2", "3"]);
+  const [items, setItems] = useState<TDraggableItems>({
+    G1: ["Player 1", "Player 2"],
+    G2: ["Player 3", "Player 4"],
+    G3: ["Player 5", "Player 6"],
+    G4: ["Player 7", "Player 8"],
+    G5: ["Player 9", "Player 10"],
+    G6: ["Player 11", "Player 12"],
+    G7: ["Player 13", "Player 14"],
+    G8: ["Player 15", "Player 16"],
+    G9: ["Player 17", "Player 18"],
+    G10: ["Player 19", "Player 20"],
+    Reserve: ["Player 21", "Player 22"],
+  });
+  const [activeId, setActiveId] = useState<string | null>(null);
   const sensors = useSensors(
     useSensor(PointerSensor),
     useSensor(KeyboardSensor, {
@@ -53,26 +101,37 @@ export default function RosterMaker() {
     <DndContext
       sensors={sensors}
       collisionDetection={closestCenter}
-      onDragEnd={(event) => {
-        const { active, over } = event;
-
-        if (active.id !== over?.id) {
-          setItems((items) => {
-            const oldIndex = items.indexOf(active.id as string);
-            const newIndex = items.indexOf(over?.id as string);
-
-            return arrayMove(items, oldIndex, newIndex);
-          });
-        }
+      onDragStart={(event) => {
+        setActiveId(event.active.id as string);
       }}
+      onDragEnd={() => setActiveId(null)}
+      // onDragEnd={(event) => {
+      //   const { active, over } = event;
+
+      //   if (active.id !== over?.id) {
+      //     setItems((items) => {
+      //       const oldIndex = items.indexOf(active.id as string);
+      //       const newIndex = items.indexOf(over?.id as string);
+
+      //       return arrayMove(items, oldIndex, newIndex);
+      //     });
+      //   }
+      // }}
     >
-      <div className="space-y-4">
-        <SortableContext items={items} strategy={verticalListSortingStrategy}>
-          {items.map((id) => (
-            <SortableItem key={id} id={id} />
-          ))}
-        </SortableContext>
+      <div className="grid grid-cols-5 gap-4">
+        {warGroups.map((name) => (
+          <GroupContainer key={name} id={name}>
+            <SortableContext items={items[name]}>
+              {items[name].map((id) => (
+                <SortableItem key={id} id={id} />
+              ))}
+            </SortableContext>
+          </GroupContainer>
+        ))}
       </div>
+      <DragOverlay>
+        {activeId ? <SortableItem id={activeId} /> : null}
+      </DragOverlay>
     </DndContext>
   );
 }
