a7237ea8189fae7eadc246cab14cbcb2fc746fca
Pujit Mehrotra <pmehrotra2@babson.edu>
2023-04-10 00:31:29 -0400

    inc: theme management via next-themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,14 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { ThemeProvider } from "next-themes";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
