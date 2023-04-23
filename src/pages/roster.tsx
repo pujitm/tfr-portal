fbee307f005694036b71c50f6ece29e09a2e524c
Pujit Mehrotra <pmehrotra2@babson.edu>
2023-04-23 17:01:49 -0600

    feat: shadcn styles + tabs on roster page

--- src/pages/roster.tsx
+++ src/pages/roster.tsx
@@ -1,9 +1,22 @@
 import { type NextPage } from "next";
 import dynamic from "next/dynamic";
+import { Label } from "@radix-ui/react-dropdown-menu";
 
 import { api } from "~/utils/api";
 import BasePage from "~/ui/BasePage";
 import DefaultHeader from "~/ui/DefaultHeader";
+import { TabsDemo } from "~/ui/TabsDemo";
+import { Button } from "~/ui/primitive/Button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/ui/primitive/card";
+import { Input } from "~/ui/primitive/input";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/ui/primitive/tabs";
 
 const RosterMaker = dynamic(() => import("../ui/RosterMaker"), { ssr: false });
 
@@ -26,7 +39,30 @@ const RosterPage: NextPage = () => {
       }
     >
       <div className="w-full px-8 py-12 md:px-16 xl:px-24">
-        <RosterMaker />
+        {/* <TabsDemo /> */}
+        <Tabs defaultValue="roster" className="">
+          <TabsList className="grid grid-cols-2 w-[400px]">
+            <TabsTrigger value="players">Players</TabsTrigger>
+            <TabsTrigger value="roster">Roster</TabsTrigger>
+          </TabsList>
+          <TabsContent value="players">
+            <Card>
+              <CardHeader>
+                <CardTitle>Player List</CardTitle>
+                <CardDescription>
+                  List of all players with various sorting views
+                </CardDescription>
+              </CardHeader>
+              <CardContent className="space-y-2">Hi</CardContent>
+              <CardFooter>
+                <Button>Save changes</Button>
+              </CardFooter>
+            </Card>
+          </TabsContent>
+          <TabsContent value="roster">
+            <RosterMaker />
+          </TabsContent>
+        </Tabs>
       </div>
     </BasePage>
   );
